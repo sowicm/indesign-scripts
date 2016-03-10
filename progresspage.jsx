5ae6419fc08ccbf2803de79f5add445f0a97a616
sowicm <sowicm@Sowicm-MacBook-Air.local>
2016-03-10 19:03:05 +0800

    绘制圆角

--- progresspage.jsx
+++ progresspage.jsx
@@ -38,7 +38,75 @@ function main()
 			var y2 = y1 + progress_height;
 			var x1 = progress_x1;
 			var x2 = x1 + progress_width;
-			mypage.rectangles.add(undefined, undefined, undefined, {geometricBounds: [y1 + 'mm', x1 + 'mm', y2 + 'mm', x2 + 'mm'], contentType: ContentType.unassigned});
+			var rect = mypage.rectangles.add(undefined, undefined, undefined, {geometricBounds: [y1 + 'mm', x1 + 'mm', y2 + 'mm', x2 + 'mm'], contentType: ContentType.unassigned});
+
+			var myPathPoint, myPoint, myPointA, myPointB, myPointC, myAnchor, myX, myY, myX1, myY1, myX2, myY2, myX3, myY3;
+			var myNewX1, myNewY1, myNewX2, myNewY2, myXOffset, myYOffset, myPoint, myPathPoint;
+			var myOffset = 5;// mm "12 pt";
+			for (var ipath = 0; ipath < rect.paths.length; ++ipath)
+			{
+				var path = rect.paths.item(ipath);
+				var point_array = new Array;
+				for (var ipoint = 0; ipoint < path.pathPoints.length; ++ipoint)
+				{
+					if ((j == 0 && (ipoint == 0 || ipoint == 3)) ||
+						(j == 1 && (ipoint == 1 || ipoint == 2)) )
+					{
+						myPointA = path.pathPoints.item(ipoint);
+						myAnchor = myPointA.anchor;
+						myX1 = myAnchor[0];
+						myY1 = myAnchor[1];
+						//myPointB is the *next* point on the path. If myPathPoint is the last point on the path, then
+						//myPointB is the first point on the path.
+						if (ipoint == (path.pathPoints.length - 1)){
+							myPointB = path.pathPoints.item(0);
+						}
+						else{
+							myPointB = path.pathPoints.item(ipoint + 1);
+						}
+						myAnchor = myPointB.anchor;
+						myX2 = myAnchor[0];
+						myY2 = myAnchor[1];
+						//myPointC is the *previous* point on the path. If myPathPoint is the first point on the path,
+						//then myPointC is the last point on the path.
+						if (ipoint == 0){
+							myPointC = path.pathPoints.item(path.pathPoints.length - 1);
+						}
+						else{
+							myPointC = path.pathPoints.item((ipoint - 1) % path.pathPoints.length);
+						}
+						myAnchor = myPointC.anchor;
+						myX3 = myAnchor[0];
+						myY3 = myAnchor[1];
+						var myPoints = myAddPoints(myX1, myY1, myX2, myY2, myX3, myY3, myOffset);
+						myNewX1 = myPoints[0];
+						myNewY1 = myPoints[1];
+						myNewX2 = myPoints[2];
+						myNewY2 = myPoints[3];
+						//Calculate new path point values based on the path effect type.
+						//We won't add the points to the path one at a time; instead, we'll
+						//create an array that holds all of the point locations and curve
+						//handle positions, and we will then 
+						//Rounded corner effect.
+						myPoint = [[myNewX2, myNewY2], [myNewX2, myNewY2], [myX1, myY1]];
+						point_array.push(myPoint);
+						myPoint = [[myNewX1, myNewY1], [myNewX1, myNewY1], [myNewX1, myNewY1]];
+						point_array.push(myPoint);
+					}
+					else
+					{
+						myX1 = path.pathPoints.item(ipoint).leftDirection[0];
+						myY1 = path.pathPoints.item(ipoint).leftDirection[1];
+						myX2 = path.pathPoints.item(ipoint).anchor[0];
+						myY2 = path.pathPoints.item(ipoint).anchor[1];
+						myX3 = path.pathPoints.item(ipoint).rightDirection[0];
+						myY3 = path.pathPoints.item(ipoint).rightDirection[1];
+						myPoint = [[myX1, myY1], [myX2, myY2], [myX3, myY3]];
+						point_array.push(myPoint);
+					}
+				}
+				path.entirePath = point_array;
+			}
 		}
 	}
 
@@ -126,3 +194,47 @@ function myGetBounds(myDocument, myPage){
 	var myY2 = myPageHeight - myPage.marginPreferences.bottom;
 	return [myY1, myX1, myY2, myX2];
 }
+
+function myAddPoints(myX1, myY1, myX2, myY2, myX3, myY3, myOffset){
+	var myXAdjust, myYAdjust, myNewX1, myNewY1, myNewX2, myNewY2, myHypotenuse;
+	myHypotenuse = Math.sqrt(Math.pow((myX1 - myX2),2) + Math.pow((myY1 - myY2),2));
+	if (myY1 != myY2) {
+		myXAdjust = ((myX1 - myX2) / myHypotenuse) * myOffset;
+		myYAdjust = ((myY1 - myY2) / myHypotenuse) * myOffset;
+		myNewX1 = myX1 - myXAdjust;
+		myNewY1 = myY1 - myYAdjust;
+	}
+	else {
+		myXAdjust = myOffset;
+		myYAdjust = 0;
+		if (myX1 < myX2) {
+			myNewX1 = myX1 + myXAdjust;
+			myNewY1 = myY1 + myYAdjust;
+		}
+		else{
+			myNewX1 = myX1 - myXAdjust;
+			myNewY1 = myY1 - myYAdjust;
+		}
+	}
+	myHypotenuse = Math.sqrt(Math.pow((myX1 - myX3),2) + Math.pow((myY1 - myY3),2));
+	if (myY1 != myY3) {
+		myXAdjust = ((myX1 - myX3) / myHypotenuse) * myOffset;
+		myYAdjust = ((myY1 - myY3) / myHypotenuse) * myOffset;
+		myNewX2 = myX1 - myXAdjust;
+		myNewY2 = myY1 - myYAdjust;
+	}
+	else{
+		myXAdjust = myOffset;
+		myYAdjust = 0;
+		if (myX1 < myX3) {
+			myNewX2 = myX1 + myXAdjust;
+			myNewY2 = myY1 + myYAdjust;
+		}
+		else{
+			myNewX2 = myX1 - myXAdjust;
+			myNewY2 = myY1 - myYAdjust;
+		}
+	}
+	return [myNewX1, myNewY1, myNewX2, myNewY2];
+}
+
